22b1e66d4703a3a2b9159182fa5a80a9066040f7
Patrycja Śliż-Szpytma <slizpatrycja@gmail.com>
2022-03-14 21:03:54 +0100

    adding functions to JS code

--- js/form.js
+++ js/form.js
@@ -1,46 +1,47 @@
-
-let button = document.querySelector(".js_button");
-
-let amountElement = document.querySelector(".js-amount"); //wprowadzana kwota
-let formElement = document.querySelector(".js-form");
-let firstSelectElement = document.querySelector(".js-selectFirst"); //pierwsza waluta
-let secondSelectElement = document.querySelector(".js-selectSecond"); //druga waluta
-let resultElement = document.querySelector(".js-value"); //wynik - element a nie kwota jako łancuch znaków
-
-let rateEUR = 4.4278;
-
-formElement.addEventListener("submit", (event) => {
-
-    event.preventDefault();
-
-    let amount = +amountElement.value;
-    let currency1 = firstSelectElement.value;
-    let currency2 = secondSelectElement.value;
-
-    let result; // kwota jako łańcuch znaków lub liczba
-
+{
+const calculateResult=(amount, currency1, currency2) =>{
+    const rateEUR = 4.4278;
     switch (currency1) {
         case "PLN":
-            if (currency2 === "EUR") {
-                result = amount / rateEUR;
-                //result.innerText = calculation.toFixed(2);
-            } else {
-                result = amount
-                //resultElement.innerText = "Błędnie wybrana waluta"
+            if (currency2 === "EUR"){
+                return  amount / rateEUR;
+            }else{
+                return  amount;
             }
-            break;
         case "EUR":
-            if (currency2 === "PLN") {
-                result = amount * rateEUR;
-                // result.innerText = calculation.toFixed(2);
-            } else {
-                result = amount
-                //resultElement.innerText = "Błędnie wybrana waluta"
+            if (currency2 === "PLN"){
+                return amount * rateEUR;
+            }else{
+                return  amount;
             }
-            break;
         default:
             resultElement.innerText = "Nie wprowadzono danych"
     }
+};
 
+const updatedResultText=(amount, result, currency1, currency2) =>{
+    const resultElement = document.querySelector(".js-value"); //wynik - element a nie kwota jako łancuch znaków
     resultElement.innerText = `${amount.toFixed(2)} ${currency1} po przewalutowaniu to ${result.toFixed(2)} ${currency2}`
-});
+};
+
+const formSubmitted = (event) =>{
+    event.preventDefault();
+    const amountElement = document.querySelector(".js-amount"); //wprowadzana kwota
+    const firstSelectElement = document.querySelector(".js-selectFirst"); //pierwsza waluta
+    const secondSelectElement = document.querySelector(".js-selectSecond"); //druga waluta  
+
+    const amount = +amountElement.value;
+    const currency1 = firstSelectElement.value;
+    const currency2 = secondSelectElement.value;
+    const result=calculateResult(amount, currency1, currency2); // kwota jako łańcuch znaków lub liczba
+    updatedResultText(amount, result, currency1, currency2);
+};
+
+const init = () => {        
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", formSubmitted);
+    };
+
+init();
+    
+}
